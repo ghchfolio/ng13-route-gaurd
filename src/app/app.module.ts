3efdb217342db1bdd9b4ad4946929e66a9444265
ghchfolio <ghchfolio@gmail.com>
2022-05-10 21:24:09 -0500

    created basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FruitsComponent } from './components/fruits/fruits.component';
+import { VeggiesComponent } from './components/veggies/veggies.component';
+import { LogInComponent } from './components/log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FruitsComponent,
+    VeggiesComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,

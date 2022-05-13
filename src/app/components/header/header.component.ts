fa9c877d65ce0ecc44cd359e67683dc1b52eeda3
ghchfolio <ghchfolio@gmail.com>
2022-05-13 01:17:26 -0500

    added route guard logic

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+    signedIn: boolean | undefined;
+    constructor(private us: UserService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.signedIn = this.us.isSignedIn();
+    }
 
 }
